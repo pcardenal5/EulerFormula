517606220c6d471b31a5c2b3db41d541ecbdfe6c
pcardenal5 <91983187+pcardenal5@users.noreply.github.com>
2021-10-05 20:37:00 +0200

    Update Fórmula de Euler.py

--- Fórmula de Euler.py
+++ Fórmula de Euler.py
@@ -1,13 +1,16 @@
 import math
 
-
-U=(2*math.pi/3)/2
-for i in [1,2,4,6]:
-    for j in [1,2,4,6]:
-        V=(2*math.pi/i)/2
-        W=(2*math.pi/j)/2
-        c=(math.cos(W)+math.cos(U)*math.cos(V))/(math.sin(U)*math.sin(V))
-        if abs(c)<=1:
-            w=math.acos(c)
-            print("Los ejes a", round(360*U/math.pi), "y", round(360*V/math.pi), "combinan para formar un eje de", round(360*W/math.pi), "grados, cruzandose a", 180*w/math.pi,"grados")
-            print(c*c)
+prueba="0"
+while prueba=="0":
+    U=input("Introduzca el orden de su rotación:")
+    U=(2*math.pi/int(U))/2
+    for i in [1,2,3,4,6]:
+        for j in [1,2,3,4,6]:
+            V=(2*math.pi/i)/2
+            W=(2*math.pi/j)/2
+            c=(math.cos(W)+math.cos(U)*math.cos(V))/(math.sin(U)*math.sin(V))
+            if abs(c)<=1:
+                w=math.acos(c)
+                print("Los ejes a", round(360*U/math.pi), "y", round(360*V/math.pi), "combinan para formar un eje de", round(360*W/math.pi), "grados, cruzandose a", 180*w/math.pi,"grados.")
+                print("cos^2(w)=",c*c)
+    prueba=input("Si desea finalizar el programa, introduzca 0. De lo contrario, introduzca cualquier otra cosa")
